697b4131aa5f9321a9a923635b12d2a861a8407f
Helga <111192958+april19833@users.noreply.github.com>
2024-07-02 22:51:58 +0300

    Update scripts.js
    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,30 +1,4 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', function() {
-    const postsSection = document.getElementById('posts');
+    const darkModeToggle = document.getElementById('darkModeToggle');
 
-    const posts = [
-        {
-            title: 'Мой первый пост',
-            content: 'Это содержимое моего первого поста.'
-        },
-        {
-            title: 'Еще один пост',
-            content: 'Вот еще немного контента для моего блога.'
-        }
-    ];
-
-    posts.forEach(post => {
-        const postElement = document.createElement('div');
-        postElement.classList.add('post');
-
-        const postTitle = document.createElement('h2');
-        postTitle.textContent = post.title;
-        postElement.appendChild(postTitle);
-
-        const postContent = document.createElement('p');
-        postContent.textContent = post.content;
-        postElement.appendChild(postContent);
-
-        postsSection.appendChild(postElement);
-    });
-});
+    darkModeToggle
