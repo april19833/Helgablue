fee821cd610c2d90ebe9ea7e22d42156fba25d0b
Helga <111192958+april19833@users.noreply.github.com>
2024-07-17 21:20:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -110,9 +110,3 @@ donateBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
   modal.style.display = 'none';
 });
-
-window.addEventListener('click', function(event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-});
