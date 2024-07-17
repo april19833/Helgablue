1a9271692b92da8e1386dedda6ea0140e7c0b7f7
Helga <111192958+april19833@users.noreply.github.com>
2024-07-18 01:59:50 +0300

    Update particleEngine.js

--- particleEngine.js
+++ particleEngine.js
@@ -1,172 +1,325 @@
-// particleEngine.js
+'use strict';
 
-var ParticleEngine = (function() {
-    'use strict';
+var tetrominos = [
+    {
+        // box
+        colors: ['rgb(59,84,165)', 'rgb(118,137,196)', 'rgb(79,111,182)'],
+        data: [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // stick
+        colors: ['rgb(214,30,60)', 'rgb(241,108,107)', 'rgb(236,42,75)'],
+        data: [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // z
+        colors: ['rgb(88,178,71)', 'rgb(150,204,110)', 'rgb(115,191,68)'],
+        data: [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 1, 1],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // T
+        colors: ['rgb(62,170,212)', 'rgb(120,205,244)', 'rgb(54,192,240)'],
+        data: [
+            [0, 0, 0, 0],
+            [0, 1, 1, 1],
+            [0, 0, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // s
+        colors: ['rgb(236,94,36)', 'rgb(234,154,84)', 'rgb(228,126,37)'],
+        data: [
+            [0, 0, 0, 0],
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // backwards L
+        colors: ['rgb(220,159,39)', 'rgb(246,197,100)', 'rgb(242,181,42)'],
+        data: [
+            [0, 0, 1, 0],
+            [0, 0, 1, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    },
+    {
+        // L
+        colors: ['rgb(158,35,126)', 'rgb(193,111,173)', 'rgb(179,63,151)'],
+        data: [
+            [0, 1, 0, 0],
+            [0, 1, 0, 0],
+            [0, 1, 1, 0],
+            [0, 0, 0, 0]
+        ]
+    }
+];
 
-    function ParticleEngine(canvas_id) {
-        if (!(this instanceof ParticleEngine)) {
-            return new ParticleEngine(args);
-        }
-        
-        var _ParticleEngine = this;
+var Tetris = function (x, y, width, height) {
+    this.posX = x || 0;
+    this.posY = y || 0;
 
-        this.canvas_id = canvas_id;
-        this.stage = new createjs.Stage(canvas_id);
-        this.totalWidth = this.canvasWidth = document.getElementById(canvas_id).width = document.getElementById(canvas_id).offsetWidth;
-        this.totalHeight = this.canvasHeight = document.getElementById(canvas_id).height = document.getElementById(canvas_id).offsetHeight;
-        this.compositeStyle = "lighter";
+    this.width = width || window.innerWidth;
+    this.height = height || window.innerHeight;
 
-        this.particleSettings = [
-            {id:"small", num:300, fromX:0, toX:this.totalWidth, ballwidth:3, alphamax:0.4, areaHeight:.5, color:"#F0F0F0", fill:false}, 
-            {id:"medium", num:100, fromX:0, toX:this.totalWidth, ballwidth:8, alphamax:0.3, areaHeight:1, color:"#C0C0C0", fill:true}, 
-            {id:"large", num:10, fromX:0, toX:this.totalWidth, ballwidth:30, alphamax:0.2, areaHeight:1, color:"#A0A0A0", fill:true}
-        ];
-        this.particleArray = [];
-        this.lights = [
-            {ellipseWidth:400, ellipseHeight:100, alpha:0.6, offsetX:0, offsetY:0, color:"#D0D0D0"}, 
-            {ellipseWidth:350, ellipseHeight:250, alpha:0.3, offsetX:-50, offsetY:0, color:"#B8B8B8"}, 
-            {ellipseWidth:100, ellipseHeight:80, alpha:0.2, offsetX:80, offsetY:-50, color:"#F8F8F8"}
-        ];
+    this.bgCanvas = document.createElement('canvas');
+    this.fgCanvas = document.createElement('canvas');
 
-        this.stage.compositeOperation = _ParticleEngine.compositeStyle;
+    this.bgCanvas.width = this.fgCanvas.width = this.width;
+    this.bgCanvas.height = this.fgCanvas.height = this.height;
 
-        function drawBgLight() {
-            var light;
-            var bounds;
-            var blurFilter;
-            for (var i = 0, len = _ParticleEngine.lights.length; i < len; i++) {                
-                light = new createjs.Shape();
-                light.graphics.beginFill(_ParticleEngine.lights[i].color).drawEllipse(0, 0, _ParticleEngine.lights[i].ellipseWidth, _ParticleEngine.lights[i].ellipseHeight);
-                light.regX = _ParticleEngine.lights[i].ellipseWidth / 2;
-                light.regY = _ParticleEngine.lights[i].ellipseHeight / 2; 
-                light.y = light.initY = _ParticleEngine.totalHeight / 2 + _ParticleEngine.lights[i].offsetY;
-                light.x = light.initX = _ParticleEngine.totalWidth / 2 + _ParticleEngine.lights[i].offsetX;
+    this.bgCtx = this.bgCanvas.getContext('2d');
+    this.fgCtx = this.fgCanvas.getContext('2d');
 
-                blurFilter = new createjs.BlurFilter(_ParticleEngine.lights[i].ellipseWidth, _ParticleEngine.lights[i].ellipseHeight, 1);
-                bounds = blurFilter.getBounds();
-                light.filters = [blurFilter];
-                light.cache(bounds.x - _ParticleEngine.lights[i].ellipseWidth / 2, bounds.y - _ParticleEngine.lights[i].ellipseHeight / 2, bounds.width * 2, bounds.height * 2);
-                light.alpha = _ParticleEngine.lights[i].alpha;
+    this.bgCanvas.style.left = this.posX + 'px';
+    this.bgCanvas.style.top = this.posY + 'px';
 
-                light.compositeOperation = "screen";
-                _ParticleEngine.stage.addChildAt(light, 0);
+    this.fgCanvas.style.left = this.posX + 'px';
+    this.fgCanvas.style.top = this.posY + 'px';
 
-                _ParticleEngine.lights[i].elem = light;
+    document.body.appendChild(this.bgCanvas);
+    document.body.appendChild(this.fgCanvas);
+    this.init();
+};
+
+Tetris.prototype.init = function () {
+    this.curPiece = {
+        data: null,
+        colors: [0, 0, 0],
+        x: 0,
+        y: 0,
+    };
+
+    this.lastMove = Date.now();
+    this.curSpeed = 50 + Math.random() * 50;
+    this.unitSize = 20;
+    this.linesCleared = 0;
+    this.level = 0;
+    this.loseBlock = 0;
+
+    // init the board
+    this.board = [];
+    this.boardWidth = Math.floor(this.width / this.unitSize);
+    this.boardHeight = Math.floor(this.height / this.unitSize);
+
+    var board = this.board,
+        boardWidth = this.boardWidth,
+        boardHeight = this.boardHeight,
+        halfHeight = boardHeight / 2,
+        curPiece = this.curPiece,
+        x = 0, y = 0;
+
+    // init board
+    for (x = 0; x <= boardWidth; x++) {
+        board[x] = [];
+        for (y = 0; y <= boardHeight; y++) {
+
+            board[x][y] = {
+                data: 0,
+                colors: ['rgb(0,0,0)', 'rgb(0,0,0)', 'rgb(0,0,0)']
+            };
+
+            if (Math.random() > 0.15 && y > halfHeight) {
+                board[x][y] = {
+                    data: 1,
+                    colors: tetrominos[Math.floor(Math.random() * tetrominos.length)].colors
+                };
             }
-
-            TweenMax.fromTo(_ParticleEngine.lights[0].elem, 10, {scaleX:1.5, x:_ParticleEngine.lights[0].elem.initX, y:_ParticleEngine.lights[0].elem.initY},{yoyo:true, repeat:-1, ease:Power1.easeInOut, scaleX:2, scaleY:0.7});
-            TweenMax.fromTo(_ParticleEngine.lights[1].elem, 12, { x:_ParticleEngine.lights[1].elem.initX, y:_ParticleEngine.lights[1].elem.initY},{delay:5, yoyo:true, repeat:-1, ease:Power1.easeInOut, scaleY:2, scaleX:2, y:_ParticleEngine.totalHeight / 2 - 50, x:_ParticleEngine.totalWidth / 2 + 100});
-            TweenMax.fromTo(_ParticleEngine.lights[2].elem, 8, { x:_ParticleEngine.lights[2].elem.initX, y:_ParticleEngine.lights[2].elem.initY},{delay:2, yoyo:true, repeat:-1, ease:Power1.easeInOut, scaleY:1.5, scaleX:1.5, y:_ParticleEngine.totalHeight / 2, x:_ParticleEngine.totalWidth / 2 - 200});
         }
-        
-        var blurFilter;
-        function drawParticles() {
-            for (var i = 0, len = _ParticleEngine.particleSettings.length; i < len; i++) {
-                var ball = _ParticleEngine.particleSettings[i];
-                var circle;
-                for (var s = 0; s < ball.num; s++) {
-                    circle = new createjs.Shape();
-                    if (ball.fill) {
-                        circle.graphics.beginFill(ball.color).drawCircle(0, 0, ball.ballwidth);
-                        blurFilter = new createjs.BlurFilter(ball.ballwidth / 2, ball.ballwidth / 2, 1);
-                        circle.filters = [blurFilter];
-                        var bounds = blurFilter.getBounds();
-                        circle.cache(-50 + bounds.x, -50 + bounds.y, 100 + bounds.width, 100 + bounds.height);
-                    } else {
-                        circle.graphics.beginStroke(ball.color).setStrokeStyle(1).drawCircle(0, 0, ball.ballwidth);
+    }
+
+    // collapse the board a bit
+    for (x = 0; x <= boardWidth; x++) {
+        for (y = boardHeight - 1; y > -1; y--) {
+
+            if (board[x][y].data === 0 && y > 0) {
+                for (var yy = y; yy > 0; yy--) {
+                    if (board[x][yy - 1].data) {
+
+                        board[x][yy].data = 1;
+                        board[x][yy].colors = board[x][yy - 1].colors;
+
+                        board[x][yy - 1].data = 0;
+                        board[x][yy - 1].colors = ['rgb(0,0,0)', 'rgb(0,0,0)', 'rgb(0,0,0)'];
                     }
-                    circle.alpha = range(0, 0.1);
-                    circle.alphaMax = ball.alphamax;
-                    circle.distance = ball.ballwidth * 2;
-                    circle.ballwidth = ball.ballwidth;
-                    circle.flag = ball.id;
-                    _ParticleEngine.applySettings(circle, ball.fromX, ball.toX, ball.areaHeight);
-                    circle.speed = range(2, 10);
-                    circle.y = circle.initY;
-                    circle.x = circle.initX;
-                    circle.scaleX = circle.scaleY = range(0.3, 1);
-                    _ParticleEngine.stage.addChild(circle);
-                    animateBall(circle);
-                    _ParticleEngine.particleArray.push(circle);
                 }
             }
         }
-
-        this.applySettings = function(circle, positionX, totalWidth, areaHeight) {
-            circle.speed = range(1, 3);
-            circle.initY = weightedRange(0, _ParticleEngine.totalHeight, 1, [_ParticleEngine.totalHeight * (2 - areaHeight / 2) / 4, _ParticleEngine.totalHeight * (2 + areaHeight / 2) / 4], 0.8);
-            circle.initX = weightedRange(positionX, totalWidth, 1, [positionX + ((totalWidth - positionX)) / 4, positionX + ((totalWidth - positionX)) * 3 / 4], 0.6);
-        }
-
-        function animateBall(ball) {
-            var scale = range(0.3, 1);
-            var xpos = range(ball.initX - ball.distance, ball.initX + ball.distance);
-            var ypos = range(ball.initY - ball.distance, ball.initY + ball.distance);
-            var speed = ball.speed;
-            TweenMax.to(ball, speed, {scaleX: scale, scaleY: scale, x: xpos, y: ypos, onComplete: animateBall, onCompleteParams: [ball], ease: Linear.easeNone});
-            TweenMax.to(ball, speed / 2, {alpha: range(0, ball.alphaMax), yoyo: true, repeat: 1, ease: Linear.easeNone});
-        }
-
-        function range(min, max) {
-            return min + (max - min) * Math.random();
-        }
-
-        function weightedRange(to, from, decimalPlaces, weightedRange, weightStrength) {
-            if (to === from) return (to);
-
-            var ret;
-            if (weightStrength <= 0) ret = Math.random() * (to - from);
-            else {
-                ret = Math.random() * (to - from);
-                var numWeights = weightedRange.length;
-                var s = Math.floor(Math.random() * (numWeights + weightStrength));
-                if (s < numWeights) ret = weightedRange[s];
-            }
-            return (Math.round(ret * (10 * decimalPlaces)) / (10 * decimalPlaces)) + from;
-        }
-
-        drawBgLight();
-        drawParticles();
-
-        createjs.Ticker.timingMode = createjs.Ticker.RAF;
-        createjs.Ticker.addEventListener("tick", _ParticleEngine.stage);
-
-        window.addEventListener('resize', function() {
-            _ParticleEngine.stage.canvas.width = _ParticleEngine.totalWidth = _ParticleEngine.canvasWidth = document.getElementById(_ParticleEngine.canvas_id).width = document.getElementById(_ParticleEngine.canvas_id).offsetWidth;
-            _ParticleEngine.stage.canvas.height = _ParticleEngine.totalHeight = _ParticleEngine.canvasHeight = document.getElementById(_ParticleEngine.canvas_id).height = document.getElementById(_ParticleEngine.canvas_id).offsetHeight;
-            _ParticleEngine.applySettings();
-        });
     }
 
-    return ParticleEngine;
-}());
+    var self = this;
 
-var particleEngine = new ParticleEngine("projector");
-document.addEventListener('DOMContentLoaded', function() {
-    var mainButton = document.getElementById('mainButton');
-    var modal = document.getElementById('modal');
-    var closeButton = document.getElementById('close');
-
-    mainButton.addEventListener('click', function() {
-        modal.style.display = 'block';
+    window.addEventListener('keydown', function (e) {
+        switch (e.keyCode) {
+            case 37:
+                if (self.checkMovement(curPiece, -1, 0)) {
+                    curPiece.x--;
+                }
+                break;
+            case 39:
+                if (self.checkMovement(curPiece, 1, 0)) {
+                    curPiece.x++;
+                }
+                break;
+            case 40:
+                if (self.checkMovement(curPiece, 0, 1)) {
+                    curPiece.y++;
+                }
+                break;
+            case 32:
+            case 38:
+                curPiece.data = self.rotateTetrimono(curPiece);
+                break;
+        }
     });
 
-    closeButton.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
+    // render the board
+    this.checkLines();
+    this.renderBoard();
 
-    var donateButtons = document.getElementsByClassName('donate-button');
-    for (var i = 0; i < donateButtons.length; i++) {
-        donateButtons[i].addEventListener('click', function() {
-            var amount = this.getAttribute('data-amount');
-            var url = this.getAttribute('data-url');
-            if (url) {
-                window.location.href = url;
-            } else {
-                // Handle custom donation logic here
-                console.log('Custom donation amount: $' + amount);
+    // assign the current piece
+    this.curPiece = {
+        data: tetrominos[Math.floor(Math.random() * tetrominos.length)].data,
+        colors: tetrominos[Math.floor(Math.random() * tetrominos.length)].colors,
+        x: Math.floor(this.boardWidth / 2),
+        y: 0
+    };
+
+    // render the new piece
+    this.renderBlock();
+};
+
+Tetris.prototype.checkMovement = function (piece, x, y) {
+    var block = piece.data,
+        board = this.board,
+        boardWidth = this.boardWidth,
+        boardHeight = this.boardHeight;
+
+    for (var px = 0; px < 4; px++) {
+        for (var py = 0; py < 4; py++) {
+
+            if (block[py][px]) {
+
+                var nx = piece.x + px + x;
+                var ny = piece.y + py + y;
+
+                if (nx < 0 || nx >= boardWidth || ny >= boardHeight) {
+                    return false;
+                }
+
+                if (ny < 0) {
+                    continue;
+                }
+
+                if (board[nx][ny].data) {
+                    return false;
+                }
             }
-            modal.style.display = 'none';
-        });
+        }
     }
+    return true;
+};
+
+Tetris.prototype.rotateTetrimono = function (block) {
+    var tempBlock = [], newBlock = [];
+
+    for (var row = 0; row < 4; row++) {
+        newBlock[row] = [];
+        for (var col = 0; col < 4; col++) {
+            newBlock[row][col] = block.data[3 - col][row];
+        }
+    }
+
+    return newBlock;
+};
+
+Tetris.prototype.checkLines = function () {
+    var board = this.board,
+        boardWidth = this.boardWidth,
+        boardHeight = this.boardHeight,
+        lines = 0;
+
+    for (var y = boardHeight - 1; y > -1; y--) {
+
+        var isComplete = true;
+        for (var x = 0; x < boardWidth; x++) {
+            if (!board[x][y].data) {
+                isComplete = false;
+            }
+        }
+
+        if (isComplete) {
+            for (var yy = y; yy > 0; yy--) {
+                for (var xx = 0; xx < boardWidth; xx++) {
+
+                    board[xx][yy].data = board[xx][yy - 1].data;
+                    board[xx][yy].colors = board[xx][yy - 1].colors;
+                }
+            }
+            y++;
+            lines++;
+        }
+    }
+};
+
+Tetris.prototype.renderBoard = function () {
+    var board = this.board,
+        boardWidth = this.boardWidth,
+        boardHeight = this.boardHeight,
+        boardColor = ['rgb(0,0,0)', 'rgb(10,10,10)', 'rgb(0,0,0)', 'rgb(0,0,0)'],
+        block;
+
+    for (var x = 0; x < boardWidth; x++) {
+        for (var y = 0; y < boardHeight; y++) {
+
+            block = board[x][y];
+            this.bgCtx.fillStyle = boardColor[block.data];
+            this.bgCtx.fillRect(x * this.unitSize, y * this.unitSize, this.unitSize, this.unitSize);
+        }
+    }
+};
+
+Tetris.prototype.renderBlock = function () {
+    var block = this.curPiece.data,
+        x = this.curPiece.x,
+        y = this.curPiece.y,
+        colors = this.curPiece.colors;
+
+    for (var px = 0; px < 4; px++) {
+        for (var py = 0; py < 4; py++) {
+
+            if (block[py][px]) {
+
+                this.fgCtx.fillStyle = colors[0];
+                this.fgCtx.fillRect((x + px) * this.unitSize, (y + py) * this.unitSize, this.unitSize, this.unitSize);
+                this.fgCtx.fillStyle = colors[1];
+                this.fgCtx.fillRect((x + px) * this.unitSize + 2, (y + py) * this.unitSize + 2, this.unitSize - 4, this.unitSize - 4);
+                this.fgCtx.fillStyle = colors[2];
+                this.fgCtx.fillRect((x + px) * this.unitSize + 4, (y + py) * this.unitSize + 4, this.unitSize - 8, this.unitSize - 8);
+            }
+        }
+    }
+};
+
+// start the game
+window.addEventListener('load', function () {
+    new Tetris();
 });
