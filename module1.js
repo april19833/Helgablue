708d5fbbdd58620c493ef00dce0be2d1ce866745
Helga ₿lue 癸亥 <111192958+april19833@users.noreply.github.com>
2024-07-20 00:49:19 +0300

    Update module1.js

--- module1.js
+++ module1.js
@@ -1,105 +1,69 @@
-particlesJS('particles-js', {
-    "particles": {
-        "number": {
-            "value": 80,
-            "density": {
-                "enable": true,
-                "value_area": 800
-            }
-        },
-        "color": {
-            "value": "#ffffff"
-        },
-        "shape": {
-            "type": "circle",
-            "stroke": {
-                "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 5
-            }
-        },
-        "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-                "enable": false,
-                "speed": 1,
-                "opacity_min": 0.1,
-                "sync": false
-            }
-        },
-        "size": {
-            "value": 3,
-            "random": true,
-            "anim": {
-                "enable": false,
-                "speed": 40,
-                "size_min": 0.1,
-                "sync": false
-            }
-        },
-        "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": 6,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-            }
-        }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "repulse"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            },
-            "resize": true
-        },
-        "modes": {
-            "grab": {
-                "distance": 400,
-                "line_linked": {
-                    "opacity": 1
-                }
-            },
-            "bubble": {
-                "distance": 400,
-                "size": 40,
-                "duration": 2,
-                "opacity": 8,
-                "speed": 3
-            },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
-            },
-            "push": {
-                "particles_nb": 4
-            },
-            "remove": {
-                "particles_nb": 2
-            }
-        }
-    },
-    "retina_detect": true
-});
+
+
+
+class FlipBook{
+	constructor(bookElem){
+		this.elems={
+			book:bookElem,
+			leaves:bookElem.querySelectorAll(".leaf"),
+			buttons:{
+				next:document.getElementById("nextPage"),
+				prev:document.getElementById("prevPage")
+			}
+		};
+		this.setupEvents();
+		this.currentPagePosition = 0;
+		this.turnPage(0);
+	}
+	setPagePosition(page,position,index){
+		var transform = "";
+		
+		transform = "translate3d(0,0,"+((position<0?1:-1)*Math.abs(index))+"px)"
+		
+		if(position<0){
+			 transform+="rotate3d(0,1,0,-180deg)";
+			page.classList.add("turned")
+		}else{
+			page.classList.remove("turned")
+		}
+		if(page.style.transform != transform){
+			page.style.transform = transform;
+		}
+	}
+	turnPage(delta){
+		this.currentPagePosition+=delta;
+		if(this.currentPagePosition < 0){
+			this.currentPagePosition = 0;
+			return;
+		}
+		if(this.currentPagePosition > this.elems.leaves.length){
+			this.currentPagePosition = this.elems.leaves.length;
+			return;
+		}
+		this.elems.leaves.forEach((page,index)=>{
+			var pageNumber = index;
+			this.setPagePosition(page,pageNumber-this.currentPagePosition,index);
+		});
+		
+		if(this.currentPagePosition == 0){
+			this.elems.buttons.prev.setAttribute("disabled","disabled");
+		}else
+		if(this.currentPagePosition == this.elems.leaves.length){
+			this.elems.buttons.next.setAttribute("disabled","disabled");
+		}else{
+			this.elems.buttons.next.removeAttribute("disabled");
+			this.elems.buttons.prev.removeAttribute("disabled");
+		}
+	}
+	setupEvents(){
+		document.getElementById("nextPage").addEventListener("click",()=>{
+			this.turnPage(1);	
+		});
+		document.getElementById("prevPage").addEventListener("click",()=>{
+			this.turnPage(-1);	
+		});
+		
+	}
+}
+
+var flipBook = new FlipBook(document.getElementById("flipbook"));
