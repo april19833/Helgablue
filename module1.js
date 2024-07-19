6b7350c0dfe2a07ddb435df06e6371303da4c1a3
Helga ₿lue 癸亥 <111192958+april19833@users.noreply.github.com>
2024-07-19 23:27:37 +0300

    Update module1.js

--- module1.js
+++ module1.js
@@ -1,17 +1,19 @@
-var modal = document.getElementById('myModal');
-var btn = document.getElementById("openModalBtn");
-var span = document.getElementsByClassName("close")[0];
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("openModalBtn");
+    var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
+    span.onclick = function() {
         modal.style.display = "none";
     }
-}
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
