71a8f469b23386e772b727bc0ae78e9ec425a085
Helga ₿lue 癸亥 <111192958+april19833@users.noreply.github.com>
2024-07-21 00:02:07 +0300

    Update module1.js

--- module1.js
+++ module1.js
@@ -1,16 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var swiper = new Swiper(".swiper", {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
     spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    speed: 1000,
-    effect: "fade",
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
     },
   });
